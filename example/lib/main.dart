ed0a858cdbfb093f1d16fd44d5f8776f43e95fd8
KristinaZoteva <kristina1994misha@gmail.com>
2022-05-14 18:32:49 +0300

    Chore: upgrade flutter version in example.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     _scrollController.addListener(_scrollListener);
-    WidgetsBinding.instance?.addPostFrameCallback((_) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       setState(() {});
     });
   }
